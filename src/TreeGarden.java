19270c0a1e864d350da1ff3ec6215f63f8542a86
Michaelcj10 <michaelcjames10@gmail.com>
2015-04-19 18:54:03 +0100

    first commit

---
+++ src/TreeGarden.java
@@ -0,0 +1,12 @@
+public class TreeGarden extends Garden {
+    public Plant getShade() {
+        return new Plant("Ash");
+    }
+    public Plant getCenter() {
+        return new Plant("Oak");
+    }
+    public Plant getBorder() {
+        return new Plant("Leburnam");
+    }
+
+}
